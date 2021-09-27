749af72eb0b025972c28098c3a6f59b54fb256b7
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-27 15:33:48 -0300

    iniciando o exercicio

---
+++ src/br/com/zup/Imovel.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+/*
+endereço do imóvel, o valor do aluguel, quais são seus moradores e o nome do funcionário responsável pelo imóvel
+ */
+public class Imovel {
+    private String enderecoImovel;
+    private String valorImovel;
+}
---
+++ src/br/com/zup/Main.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+/*
+Os alunos/alunas devem criar um sistema de gestão para uma imobiliária. O sistema deve permitir o cadastro de um imóvel e de todos os seus moradores. Ao final do programa, deve ser exibido o endereço do imóvel, o valor do aluguel, quais são seus moradores e o nome do funcionário responsável pelo imóvel.
+1.Entrega Mínima: O sistema permite cadastrar um imóvel com valor do aluguel, endereço, os moradores e o funcionário responsável. No final, o sistema deve exibir os dados do imóvel.
+2. Entrega Máxima: O sistema permite adicionar mais de um imóvel e exibir a lista de imóveis.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
