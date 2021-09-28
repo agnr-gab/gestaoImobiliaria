1f794d6f03c8afc6a077073da1e14df77c7da2e2
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-27 23:12:47 -0300

    Classe Moradores

--- src/br/com/zup/Moradores.java
+++ src/br/com/zup/Moradores.java
@@ -1,8 +1,23 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Moradores {
     private String nome;
     private String cpf;
     private String profissao;
     private String rendaBruta;
+
+    private List<Imoveis> listaImoveis = new ArrayList<>();
+    public void adicionarImoveis(Imoveis imovel) {
+        listaImoveis.add(imovel);
+    }
+
+    public Moradores(String nome, String cpf, String profissao, String rendaBruta) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.rendaBruta = rendaBruta;
+    }
 }
