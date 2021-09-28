d1c7389afe4661be57310c97da0f81169cb6d1ca
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-27 23:44:39 -0300

    correções na classe moradores

--- src/br/com/zup/Moradores.java
+++ src/br/com/zup/Moradores.java
@@ -1,63 +1,9 @@
 package br.com.zup;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Moradores {
-    private String nome;
-    private String cpf;
-    private String profissao;
-    private double rendaBruta;
+public class Moradores extends Pessoa {
 
     public Moradores(String nome, String cpf, String profissao, double rendaBruta) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.profissao = profissao;
-        this.rendaBruta = rendaBruta;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getProfissao() {
-        return profissao;
-    }
-
-    public void setProfissao(String profissao) {
-        this.profissao = profissao;
-    }
-
-    public double getRendaBruta() {
-        return rendaBruta;
-    }
-
-    public void setRendaBruta(double rendaBruta) {
-        this.rendaBruta = rendaBruta;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder retornoImoveis = new StringBuilder();
-
-        retornoImoveis.append("\n Nome: " + getNome());
-        retornoImoveis.append("\n CPF: " + getCpf());
-        retornoImoveis.append("\n Profissão: " + getProfissao());
-        retornoImoveis.append("\n Renda Bruta: " + getRendaBruta() + " reais");
-
-        return retornoImoveis.toString();
+        super(nome, cpf, profissao, rendaBruta);
     }
 }
 
