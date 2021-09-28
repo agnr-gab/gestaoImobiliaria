807c69012ce10b0526864e623bb051a4af62f6a2
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-27 23:20:24 -0300

    metodos acessores e toString classe Moradores

--- src/br/com/zup/Moradores.java
+++ src/br/com/zup/Moradores.java
@@ -7,17 +7,64 @@ public class Moradores {
     private String nome;
     private String cpf;
     private String profissao;
-    private String rendaBruta;
+    private double rendaBruta;
 
     private List<Imoveis> listaImoveis = new ArrayList<>();
-    public void adicionarImoveis(Imoveis imovel) {
-        listaImoveis.add(imovel);
-    }
 
-    public Moradores(String nome, String cpf, String profissao, String rendaBruta) {
+    public Moradores(String nome, String cpf, String profissao, double rendaBruta) {
         this.nome = nome;
         this.cpf = cpf;
         this.profissao = profissao;
         this.rendaBruta = rendaBruta;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public double getRendaBruta() {
+        return rendaBruta;
+    }
+
+    public void setRendaBruta(double rendaBruta) {
+        this.rendaBruta = rendaBruta;
+    }
+
+    public void adicionarImoveis(Imoveis imovel) {
+        listaImoveis.add(imovel);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retornoImoveis = new StringBuilder();
+
+        retornoImoveis.append("\n Nome: " + getNome());
+        retornoImoveis.append("\n CPF: " + getCpf());
+        retornoImoveis.append("\n Profissão: " + getProfissao());
+        retornoImoveis.append("\n Renda Bruta: " + getRendaBruta() + " reais");
+
+        return retornoImoveis.toString();
+    }
 }
+
+
