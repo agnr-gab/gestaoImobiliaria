0e6eae240324bf91c7e33da24343038733e5b8d6
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-27 23:45:28 -0300

    criação da classe Pessoa[super classe]

--- src/br/com/zup/Pessoa.java
+++ src/br/com/zup/Pessoa.java
@@ -1,2 +1,58 @@
-package br.com.zup;public class Pessoa {
+package br.com.zup;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String profissao;
+    private double rendaBruta;
+
+    public Pessoa(String nome, String cpf, String profissao, double rendaBruta) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.rendaBruta = rendaBruta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public double getRendaBruta() {
+        return rendaBruta;
+    }
+
+    public void setRendaBruta(double rendaBruta) {
+        this.rendaBruta = rendaBruta;
+    }
+    @Override
+    public String toString() {
+        StringBuilder retornoPessoa = new StringBuilder();
+
+        retornoPessoa.append("\n Nome: " + getNome());
+        retornoPessoa.append("\n CPF: " + getCpf());
+        retornoPessoa.append("\n Profissão: " + getProfissao());
+        retornoPessoa.append("\n Renda Bruta: " + getRendaBruta() + " reais");
+
+        return retornoPessoa.toString();
+    }
 }
