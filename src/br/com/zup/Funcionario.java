6c87f66caea0faaee88801eddebfc23039dd1ded
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-28 10:21:29 -0300

    toString() classe funcionario

--- src/br/com/zup/Funcionario.java
+++ src/br/com/zup/Funcionario.java
@@ -1,10 +1,30 @@
 package br.com.zup;
 
 public class Funcionario extends Pessoa {
+    private String ctps;
+
     public Funcionario() {
+
     }
 
-    public Funcionario(String nome, String cpf, String profissao, double rendaBruta) {
+    public Funcionario(String nome, String cpf, String profissao, double rendaBruta, String ctps) {
         super(nome, cpf, profissao, rendaBruta);
+        this.ctps = ctps;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retornoFuncionario = new StringBuilder();
+        retornoFuncionario.append(super.toString());
+        retornoFuncionario.append("Carteira de Trabalho " + ctps);
+        return retornoFuncionario.toString();
     }
 }
