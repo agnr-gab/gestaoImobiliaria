05f90e852506f8c11e51c820e9bc0379e63482ca
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-28 10:28:32 -0300

    Classe Moradores toString()

--- src/br/com/zup/Moradores.java
+++ src/br/com/zup/Moradores.java
@@ -1,11 +1,30 @@
 package br.com.zup;
 
 public class Moradores extends Pessoa {
+    private String telefone;
+
     public Moradores() {
+
     }
 
-    public Moradores(String nome, String cpf, String profissao, double rendaBruta) {
+    public Moradores(String nome, String cpf, String profissao, double rendaBruta, String telefone) {
         super(nome, cpf, profissao, rendaBruta);
+        this.telefone = telefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    @Override
+    public String toString() {
+        StringBuilder retornoMorador = new StringBuilder();
+        retornoMorador.append(super.toString());
+        retornoMorador.append("Telefone de Contato: " + telefone);
+        return retornoMorador.toString();
     }
 }
 
